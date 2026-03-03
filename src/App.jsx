a2b69ad900c67cdfb8341fc5c76f509e71c055be
ThiagoJoelP <thiagojoel17@hotmail.com>
2026-03-03 00:02:21 -0300

    Add src/main.jsx, App.jsx, App.css, index.css

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Gallery from './components/Gallery'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Gallery />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
